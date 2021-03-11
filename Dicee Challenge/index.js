542054b5921084baeaa67459ff7e39b766da7c48
Ravisara S. Kariyawasam <ravisara@gmail.com>
2021-03-11 14:23:03 +0000

    My solution changed inspired by the solution provided in the course.

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,21 +1,21 @@
 var randomNumber1;
 randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var leftImageFullPath = "images/dice" + randomNumber1 + ".png"
-document.getElementsByTagName("img")[0].setAttribute("src", leftImageFullPath);
+document.querySelectorAll("img")[0].setAttribute("src", leftImageFullPath);
 
 var randomNumber2;
 randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var rightImageFullPath = "images/dice" + randomNumber2 + ".png";
-document.getElementsByTagName("img")[1].setAttribute("src", rightImageFullPath);
+document.querySelectorAll("img")[1].setAttribute("src", rightImageFullPath);
+
 
 var resultText;
 if (randomNumber1 > randomNumber2) {
-  resultText = "Player 1 Wins!"
-} else if (randomNumber1 == randomNumber2) {
-  resultText = "Draw"
+  resultText = "🚩 Player 1 Wins!"
+} else if (randomNumber1 < randomNumber2) {
+  resultText = "Player 2 Wins! 🚩"
 } else {
-  resultText = "Player 2 Wins!"
+  resultText = "Draw"
 }
 
-//document.getElementsByTagName("h1")[0].textContent = resultText;
 document.querySelector("h1").innerHTML = resultText;
