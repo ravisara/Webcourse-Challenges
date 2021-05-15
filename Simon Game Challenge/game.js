c5307c0e5edda95a0590c44cf97387dc41519d41
Ravisara S. Kariyawasam <ravisara@gmail.com>
2021-05-15 14:03:02 +0100

    My answer to section 15 unit 203 of the Simon Game in previous commit...
    ...modified after seing the answer given in the course. 
    (Now the game can be played and the levels increase as before but the 
    logic is different - more simplified and streamlined.)
    
    If the sequence is wrong nothing happens 
    still.

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -17,32 +17,18 @@ $(document).keypress(function() {
 
 $(".btn").click(function() {
 
-  var idOfTheButtonClickedOn = $(this).attr("id") // would be one of "red", "blue", "green" or "yellow"
+  var colourOfButtonPressed = $(this).attr("id") // would be one of "red", "blue", "green" or "yellow"
+  userClickedPattern.push(colourOfButtonPressed);
+  playSound(colourOfButtonPressed);
+  animatePress(colourOfButtonPressed);
 
-  playSound(idOfTheButtonClickedOn);
-
-  animatePress(idOfTheButtonClickedOn);
-
-  userClickedPattern.push(idOfTheButtonClickedOn);
-
-  if (userClickedPattern.length == gamePattern.length) { // This means the user has clicked on the buttons as many times as the game pattern flashed the buttons.
-    // e.g. If the user has pressed red, green, red, yellow, the index of the last answer is 3.
-    // blue, blue, green, red
-    // find the index of the user's last answer
-    var theIndexOfLastAnswer = 0
-    for(i=0; i < buttonColours.length; i++) {
-      if(idOfTheButtonClickedOn == buttonColours[i]) {
-        theIndexOfLastAnswer = i;
-        break;
-      }
-    }
-    checkAnswer(theIndexOfLastAnswer) // If three values are in the array, the size is 3 but the index of the last element is 2
-  }
+  checkAnswer(userClickedPattern.length - 1) // index of the last answer in the user's sequence.
 
 })
 
 function nextSequence() {
 
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
 
@@ -75,18 +61,13 @@ function animatePress(currentColour) {
 
 function checkAnswer(currentLevel) {
 
-  if (gamePattern[gamePattern.length - 1] == buttonColours[currentLevel]) {
-    console.log("success");
-    //Check if the user has finished his nextSequence
-    if (gamePattern.length === userClickedPattern.length) { // User has clicked on as many times as the game pattern sequence length
-        setTimeout(function() {
-          nextSequence();
-          userClickedPattern = [] // makes the array empty again. https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
-        }, 1000)
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success")
+    if (userClickedPattern.length === gamePattern.length) { // user has finished the sequence
+      setTimeout(nextSequence, 1000);
     }
-
   } else {
-    console.log("wrong");
+    console.log("wrong")
   }
 
 }
